d74880b854a56c372042ccbfda1f7d6e4e457bb7
Robert JONES <rojones@e5r10p5.jnb.42.fr>
2016-09-06 13:19:22 +0200

    single qute added

--- ft_num_args.c
+++ ft_num_args.c
@@ -6,25 +6,27 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/01 16:26:27 by rojones           #+#    #+#             */
-/*   Updated: 2016/08/05 14:51:28 by rojones          ###   ########.fr       */
+/*   Updated: 2016/09/06 12:03:06 by rojones          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sh21.h"
 
-static int	ft_inc_word(char *line, int *i, int *qut)
+static int	ft_inc_word(char *line, int *i, int *s_qut, int *d_qut)
 {
 	int	end;
 	int	re;
+	int	qut;
 
 	re = 0;
 	end = 0;
 	while (line[*i] && end == 0)
 	{
 		re = 1;
-		ft_check_arg_case_len(line, qut, i);
-		if (line[*i] == '\0' || (ft_isspace(line[*i]) == 1 && *qut == 0) ||
-				(ft_isop(line[*i]) == 1 && *qut == 0))
+		ft_check_arg_case_len(line, s_qut, d_qut, i);
+		qut = (*s_qut || *d_qut) ? 1 : 0;
+		if (line[*i] == '\0' || (ft_isspace(line[*i]) == 1 && qut == 0) ||
+				(ft_isop(line[*i]) == 1 && qut == 0))
 			end = 1;
 		else
 			*i = *i + 1;
@@ -32,7 +34,7 @@ static int	ft_inc_word(char *line, int *i, int *qut)
 	return (re);
 }
 
-static int	ft_get_qut(char *line)
+/*static int	ft_get_qut(char *line)
 {
 	char	*line2;
 	char	*temp;
@@ -51,27 +53,29 @@ static int	ft_get_qut(char *line)
 	if (line2)
 		free(line2);
 	return (re);
-}
+}*/
 
 int			ft_num_args(char *line)
 {
 	int		re;
-	int		qut;
+	int		s_qut;
+	int		d_qut;
 	int		i;
 
 	i = 0;
-	qut = 0;
+	s_qut = 0;
+	d_qut = 0;
 	re = 0;
 	while (line[i] != '\0')
 	{
 		ft_skip_space(line, &i);
-		re += ft_inc_word(line, &i, &qut);
+		re += ft_inc_word(line, &i, &s_qut, &d_qut);
 		if (ft_isop(line[i]) == 1 && i > 0 && ft_isspace(line[i - 1]) == 0)
 			re++;
 		while (line[i] != '\0' && ft_isop(line[i]) == 1)
 			i++;
 	}
-	if (qut == 1)
-		re = ft_get_qut(line);
+//	if (qut == 1)
+//		re = ft_get_qut(line);
 	return (re);
 }
