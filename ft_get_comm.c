3eec2c14049baaf5a88547164be2219b65dffe37
Victor verdun VAN DER WATT <vivan-de@e5r9p4.jnb.42.fr>
2016-09-09 08:38:03 +0200

    fixed sorry

--- ft_get_comm.c
+++ ft_get_comm.c
@@ -6,7 +6,7 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/28 09:41:54 by rojones           #+#    #+#             */
-/*   Updated: 2016/09/08 08:06:59 by vivan-de         ###   ########.fr       */
+/*   Updated: 2016/09/09 08:36:56 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,30 @@
 
 int	contains_command(char *s)
 {
-	if (strcmp(s[0], "echo") == 0)
-		;
+	if (strcmp(s, "echo") == 0)
+		return (1);
 	else if (strcmp(s, "cd") == 0)
 		return (1);
 	else if ((strcmp(s, "setenv") == 0 || strcmp(s, "export") == 0))
 		return (1);
-	else if ((strcmp(s, "unsetenv") == 0 || strcmp(split[0], "unset") == 0))
+	else if ((strcmp(s, "unsetenv") == 0 || strcmp(s, "unset") == 0))
 		return (1);
 	return (0);
 }
 
-int	ft_get_block(char *s, int *block, int *pipe)
+int	ft_get_block(char **s, int *block, int *pipe, t_data *data)
 {
-	if (strcmp(s, "cd") == 0 && (*block += 1) && *pipe == 1)
-		return (ft_cd(split, data));
-	else if ((strcmp(s, "setenv") == 0 ||
-			strcmp(s, "export") == 0) && (*block += 1) && *pipe == 1)
-		return (ft_setenv(split, data));
-	else if ((strcmp(s, "unsetenv") == 0 ||
-			strcmp(split[0], "unset") == 0) && (*block += 1) && *pipe == 1)
-		return (ft_unsetenv(split, data));
+	if (strcmp(s[0], "echo") == 0)
+		return (ft_echo(s, data));
+	if (strcmp(s[0], "cd") == 0 && (*block += 1) && *pipe == 1)
+		return (ft_cd(s, data));
+	else if ((strcmp(s[0], "setenv") == 0 ||
+			strcmp(s[0], "export") == 0) && (*block += 1) && *pipe == 1)
+		return (ft_setenv(s, data));
+	else if ((strcmp(s[0], "unsetenv") == 0 ||
+			strcmp(s[0], "unset") == 0) && (*block += 1) && *pipe == 1)
+		return (ft_unsetenv(s, data));
+	return (EXIT_FAILURE);
 }
 
 int	ft_get_comm(char **s, t_data *data, int p)
@@ -48,10 +51,8 @@ int	ft_get_comm(char **s, t_data *data, int p)
 	{
 		if (strcmp(s[0], "exit") == 0)
 			ft_exit(s, data);
-		else if (strcmp(s[0], "echo") == 0)
-			return (ft_echo(s, data));
 		else if (contains_command(s[0]))
-			return (ft_get_block(s[0], &b, &p));
+			return (ft_get_block(s, &b, &p, data));
 		else if (strcmp(s[0], "env") == 0)
 			ft_env(s, data->env);
 		else if (b == 0 && (path = ft_search_path(s, data)) != NULL)
